921694a2eb5233ce0421bd59f22ea3adc2c5e51b
TristanEkaW <Tristanekaw@gmail.com>
2023-12-10 09:29:05 +0700

    Re-fixing folder

--- README.md
+++
@@ -1,9 +0,0 @@
-# Praktikum Dasar Pemrograman Kelas TI-1B
-
-Created by Tristan Eka Wiranata
-
-## Contoh Kode
-
-Kode Guess Number
-
-Weight Conversion
--- weight_conversion.py
+++
@@ -1,9 +0,0 @@
-def weight_conversion():
-    berat = int(input("Masukan berat anda > "))
-    satuan = input("Dalam satuan apa berat yang anda masukkan ? (K untuk KG, L untuk LBS) > ")
-    
-    if satuan.lower() == 'l':
-        print(f"Berat anda dikonversi menjadi kilogram adalah {round(berat * 0.453592)} kg")
-    elif satuan.lower() == 'k':
-        print(f"Berat anda dikonversi menjadi pon adalah {round(berat * 2.20462)} lbs")
-        
